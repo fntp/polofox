1f2a3ba1588a5f1d14b78a1bf11f740db46d2aa6
fntp <stickpoint@163.com>
2021-08-04 18:02:22 +0800

    fntp
    这是第一版本的polofox，后续会继续更新，目前在确定项目结构与技术架构

---
+++ polofox/src/main/java/com/polofox/PolofoxApplication.java
@@ -0,0 +1,11 @@
+package com.polofox;
+
+import java.time.LocalDateTime;
+
+public class PolofoxApplication {
+    public static void main(String[] args) {
+    //  程序启动入口
+        System.out.println("INFO:GMT:"+LocalDateTime.now()+"Modifier:fntp:scx:SunChengXin");
+
+    }
+}
